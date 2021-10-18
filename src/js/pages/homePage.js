658c3221c156966480806db1ed884666a285d3ed
Clayton-Endersby <77210291+Clayton-Endersby@users.noreply.github.com>
2021-10-18 17:24:26 -0600

    new

---
+++ src/js/pages/homePage.js
@@ -0,0 +1,16 @@
+import levelHeading from './../components/ui/levelHeading'
+import link from './../components/ui/link'
+
+const homePage = function () {
+    const app = document.querySelector('#app')
+
+    const header = document.createElement('header')
+    const h1 = levelHeading('h1', 'Home Page')
+    const login = link("To Do's", '/toDos')
+    header.append(h1)
+    header.append(toDos)
+
+    return header
+}
+
+export default homePage
---
+++ src/js/router/router.js
@@ -0,0 +1,24 @@
+import homePage from "../pages/homePage";
+import toDoPage from "../pages/toDoPage";
+
+const routes = {
+    '/':homePage(),
+    '/login':loginPage(),
+    '/signUp':signUpPage(),
+}
+
+const Router = function (pathname) {
+    const app = document.querySelector('#app')
+    app.innerHTML = ''
+
+    window.history.pushState(
+        {},
+        pathname,
+        window.location.origin + pathname
+        )
+        
+        app.appendChild(routes[window.location.pathname])
+}
+
+export {routes}
+export default Router
